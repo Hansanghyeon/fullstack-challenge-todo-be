41325e51071dd7321bc66472dbaad2acfe887cca
Hansanghyeon <999@hyeon.pro>
2024-08-01 03:05:42 +0000

    feat: dev환경에서 master 토큰으로 진행할 수 있는 환경 구축

--- src/auth/security/auth.guard.ts
+++ src/auth/security/auth.guard.ts
@@ -1,12 +1,23 @@
 import { ExecutionContext, Injectable } from '@nestjs/common'
 import { AuthGuard as NestAuthGuard } from '@nestjs/passport'
 import { Observable } from 'rxjs'
+import { ConfigService } from '@nestjs/config'
 
 @Injectable()
 export class AuthGuard extends NestAuthGuard('jwt') {
+  constructor(private configService: ConfigService) {
+    super()
+  }
+
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest()
+    const masterToken = this.configService.get<string>('MASTER_TOKEN')
+
+    if (request.headers.authorization === `Bearer ${masterToken}`) {
+      return true
+    }
     return super.canActivate(context)
   }
 }
