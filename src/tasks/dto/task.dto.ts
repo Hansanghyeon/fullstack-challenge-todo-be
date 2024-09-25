d4bb3682b942129668c816550d7d54e577025a50
Hansanghyeon <999@hyeon.pro>
2024-09-25 00:11:11 +0000

    chore: docs 업데이트

--- src/tasks/dto/task.dto.ts
+++ src/tasks/dto/task.dto.ts
@@ -1,8 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { T할일_상태 } from '~/constants/할일_상태'
 
 export class TaskDTO {
   id: number
+
+  @ApiProperty()
   title: string
+
+  @ApiProperty()
   description: string
+
   status: T할일_상태
 }
--- src/tasks/dto/user-task.dto.ts
+++ src/tasks/dto/user-task.dto.ts
@@ -1,6 +1,8 @@
 import { Task } from '~/entities/task'
 import { User } from '~/entities/user'
 
+// UserTaskDTO는 User와 Task의 관계를 나타내는 DTO이다.
+// 그래서 api로 추가할 수없기 떄문에 swagger에 payload 타입을 추가하지 않는다.
 export class UserTaskDTO {
   id: number
   user: User
